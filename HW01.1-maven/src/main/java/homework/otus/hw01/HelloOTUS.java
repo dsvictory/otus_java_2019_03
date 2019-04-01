9209c4c797a3471d77ffef621ef80543ae34f7f2
DSVictory <DSVictory@DESKTOP-LL18RAV>
2019-04-01 20:24:31 +0700

    Выполнение домашнего задания по Maven

---
+++ HW01.1-maven/src/main/java/homework/otus/hw01/HelloOTUS.java
@@ -0,0 +1,12 @@
+package homework.otus.hw01;
+
+import com.google.common.base.*;
+
+public class HelloOTUS {
+
+	public static void main(String[] args) {
+		Joiner joiner = Joiner.on("; ").skipNulls();
+		System.out.print(joiner.join("Harry", null, "Ron", "Hermione"));
+	}
+
+}
